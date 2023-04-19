bf4d08204362a282671c2656d1ef4db467ee7441
saraparveen26 <sara.parveen26@gmail.com>
2023-04-18 20:57:40 -0400

    updated athletes data for js

--- static/app.js
+++ static/app.js
@@ -136,11 +136,57 @@ init1();
 
 
 // ------- ATHLETES DATA ------- //
-// Get the data
-const athletes = "./data-json/offense.json";
+
+// Read the data files
+const offense = "./data-json/offense.json";
+const defense = "./data-json/defense.json";
+const special = "./data-json/special.json";
+
+// Get the dropdown element
+const dropdown = document.getElementById('player-dropdown');
+
+
+// Add the player names from each dataset to the dropdown
+[offense, defense, special].forEach(athletes_dataset => {
+    athletes_dataset.forEach(player => {
+      const option = document.createElement('option');
+      option.value = player.name;
+      option.text = player.name;
+      dropdown.add(option);
+    });
+  });
+
+// Add an event listener to the dropdown
+dropdown.addEventListener('change', function() {
+
+  // Get the selected player name
+  const playerName = dropdown.value;
+  
+  // Determine which dataset the player is in based on their name
+  let athletes_dataset;
+  if (offense.some(player => player.name === playerName)) {
+    athletes_dataset = offense;
+  } 
+  else if (defense.some(player => player.name === playerName)) {
+    athletes_dataset = defense;
+  } 
+  else if (special.some(player => player.name === playerName)) {
+    athletes_dataset = special;
+  } 
+  else {
+    // Handle the case where the player isn't found in any dataset
+    console.error(`Player "${playerName}" not found.`);
+  return;
+  }
+  
+  // Do something with the selected dataset, such as display a chart using D3
+  console.log(`Selected dataset for "${playerName}":`, athletes_dataset);
+});
+
+
 
 // Fetch the JSON data and console log it
-d3.json(athletes).then(function(athletes_data) {
+d3.json(athletes_dataset).then(function(athletes_data) {
     console.log(athletes_data[0]);
   });
 
@@ -152,18 +198,10 @@ function init2() {
     let dropdownMenu = d3.select("#selDataset");
 
     // Get the athlete names and populate the dropdown options
-    d3.json(athletes).then((athletes_data) => {
-        
-        // Add athlete names to the dropdown menu
-        athletes_data.forEach((athlete) => {
-
-            console.log(athlete);
-
-            dropdownMenu.append("option").text(athlete['offense_name']).property("value", athlete['offense_name']);
-        });
+    d3.json(athletes_dataset).then((athletes_data) => {
 
         // Get the first athlete
-        let firstAthlete = athletes_data[0]['offense_name'];
+        let firstAthlete = athletes_data[0]['name'];
 
         // Console log the first athlete details
         console.log(firstAthlete);
@@ -186,23 +224,23 @@ function demoInfo(athleteName) {
         
         // Retrieve all required information
         let athlete_info = {
-            'Name': filteredAthlete[0]['offense_name'], 
-            'Weight': filteredAthlete[0]['offense_weight'],
-            'Height': filteredAthlete[0]['offense_height'], 
-            'Age': filteredAthlete[0]['offense_age'],
-            'Birth City': filteredAthlete[0]['offense_birthCity'],
-            'Birth Country':filteredAthlete[0]['offense_birthCountry'], 
-            'Position':filteredAthlete[0]['offense_position'], 
-            'Experience (in years)':filteredAthlete[0]['offense_xp']
+            'Name': filteredAthlete[0]['name'], 
+            'Weight': filteredAthlete[0]['weight'],
+            'Height': filteredAthlete[0]['height'], 
+            'Age': filteredAthlete[0]['age'],
+            'Birth City': filteredAthlete[0]['birthCity'],
+            'Birth Country':filteredAthlete[0]['birthCountry'], 
+            'Position':filteredAthlete[0]['position'], 
+            'Experience (in years)':filteredAthlete[0]['xp']
         };
 
         let obj = filteredAthlete[0]
 
          // Fecht Athlete Headshot
         d3.select("#sample-logo").html("");
-        d3.select("#sample-logo").append('img').attr('src', filteredAthlete[0]['offense_headshot']).attr('alt', filteredAthlete[0]['offense_name']).attr('height', 150)
+        d3.select("#sample-logo").append('img').attr('src', filteredAthlete[0]['headshot']).attr('alt', filteredAthlete[0]['name']).attr('height', 150)
 
-        console.log(filteredAthlete[0]['offense_headshot'])
+        console.log(filteredAthlete[0]['headshot'])
 
         d3.select("#sample-metadata").html("");
          let entries = Object.entries(athlete_info);
